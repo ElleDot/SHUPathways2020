33431db2702a03adb43071db0ddb8b1b3b283cdd
Tom Paddon <tpaddon@users.noreply.github.com>
2019-03-06 13:59:16 +0000

    Timer update

--- shupathways/assets/js/custom.js
+++ shupathways/assets/js/custom.js
@@ -39,7 +39,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2019/03/06 12:00:00').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2019/03/06 11:00:00').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
